fbf44832dc34896cb33da45d4e361439209b613e
Schwarzfox <r.hosseinabadi@gmail.com>
2021-02-08 21:33:58 +0100

    very simple graph of the week average number

---
+++ Progress points_Schedule.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+week_number=(-1,0,1,2,3,4,5)
+average_points=(6.714286,8.2858,12.42857,13.28571,10.42856,7.7142,14.2857)
+plt.plot(week_number,average_points, 'go')
+plt.show()
