2ee8d8bd794b380b3ad9ef5d7ee6561362157e8d
Schwarzfox <r.hosseinabadi@gmail.com>
2021-02-08 23:21:14 +0100

    added the plot, axes titles and axes limits

--- Progress points_Schedule.py
+++ Progress points_Schedule.py
@@ -1,5 +1,15 @@
 import matplotlib.pyplot as plt
+
 week_number=(-1,0,1,2,3,4,5)
 average_points=(6.714286,8.2858,12.42857,13.28571,10.42856,7.7142,14.2857)
-plt.plot(week_number,average_points, 'go')
+average=sum(average_points)/len(average_points)
+
+plt.plot(week_number,average_points, 'bo')
+plt.title("Weekly Average Points")
+plt.ylabel("Average Points")
+plt.xlabel("Week Numbers")
+plt.xlim(-2,6)
+plt.ylim(0,20)
+
+
 plt.show()
