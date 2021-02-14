c981bda4a06a19e5d74d13f101745c26ca8e7350
Schwarzfox <r.hosseinabadi@gmail.com>
2021-02-14 18:05:06 +0100

    Learning the lists and how to access them, still from the 4 hours course from Youtube

--- Learning.py
+++ Learning.py
@@ -13,5 +13,12 @@ print(ceil(2.9))
 print(sqrt(20))
 
 
+my_list= [1,3,5, 100, 200, 800,9000, -21,6, "hoyoyo"]
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:])
+my_list[2]= "Gowje"
+print(my_list[1:4])
+
 your_name = raw_input ("enter your name here:")
-print(your_name)
+print(your_name+" is bad")
