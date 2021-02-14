45bfd6cb3dbc30827750abb34408131c52c446a2
Schwarzfox <r.hosseinabadi@gmail.com>
2021-02-14 19:27:51 +0100

    Added comments to the sections, finished the lists

--- Learning.py
+++ Learning.py
@@ -1,5 +1,4 @@
-
-
+###BASIC OPERATIONS###
 
 my_num = 5
 str(my_num)
@@ -13,12 +12,34 @@ print(ceil(2.9))
 print(sqrt(20))
 
 
-my_list= [1,3,5, 100, 200, 800,9000, -21,6, "hoyoyo"]
-print(my_list[0])
-print(my_list[-1])
-print(my_list[2:])
-my_list[2]= "Gowje"
-print(my_list[1:4])
+#your_name = raw_input ("enter your name here:") #Gets the input from userer
+# print(your_name+" is bad")
 
-your_name = raw_input ("enter your name here:")
-print(your_name+" is bad")
+###LISTS###
+my_list= [1,3,5, 100, 200, 800,9000, -21,6, 6,100,"hoyoyo"]
+name_list= ["Ad", "BC", "CD"]
+# print(my_list[0])
+# print(my_list[-1])
+# print(my_list[2:])
+# my_list[2]= "Gowje"
+# print(my_list[1:4])
+
+my_list.extend(name_list)         #Append a new list to the end
+my_list.pop()                     #Removes the last element
+print(my_list)
+# my_list.append("AppendEx!") #Append a new element to the list
+# my_list.insert(0,"Start")         #Insert an element to the list, pushes everything one step
+# my_list.remove(200)
+# print(my_list)
+
+print(my_list.index(100))         #Shows the location of the element 100
+print(my_list.count(6))           #Counts how mnay 6 we have
+# my_list.sort()                   #Sorts the list
+my_list.reverse()
+print(my_list)
+
+
+
+
+
+
